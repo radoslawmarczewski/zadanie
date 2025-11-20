20e1239e3ebd26401aee872ad06b668aba69ad9a
Radoslaw Marczewski <rmarczewski@mat.umk.pl>
2025-11-20 22:37:21 +0100

    Dodanie walidacji liczb ujemnych

--- program.cpp
+++ program.cpp
@@ -5,6 +5,11 @@ using namespace std;
 int main() {
 	int liczba;
 	cin >> liczba;
+
+	if (liczba < 0) {
+		cout << "Blad: liczba ujemna" << endl;
+		return 0;
+	}
 	cout << liczba << endl;
 	return 0;
 }
