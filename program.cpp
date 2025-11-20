880c381353fadb8975cd5f93d66d17e25baf2f9f
Radoslaw Marczewski <rmarczewski@mat.umk.pl>
2025-11-20 22:31:47 +0100

    dodanie silni

--- program.cpp
+++ program.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
+unsigned long long silnia(int n) {
+	if (n <= 1) return 1;
+	return n * silnia(n - 1);
+}
+
 int main() {
 	int liczba;
 	cin >> liczba;
-	cout << liczba << endl;
+	cout << silnia(liczba) << endl;
 	return 0;
 }
